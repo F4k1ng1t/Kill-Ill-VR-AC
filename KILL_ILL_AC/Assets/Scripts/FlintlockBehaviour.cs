78a09cce34e4a76261651ef4e30d2191ab30c0fd
F4k1ng1t <156473484+F4k1ng1t@users.noreply.github.com>
2026-04-19 13:00:08 -0700

    gun behaviour

--- KILL_ILL_AC/Assets/Scripts/FlintlockBehaviour.cs
+++ KILL_ILL_AC/Assets/Scripts/FlintlockBehaviour.cs
@@ -6,7 +6,9 @@ public class FlintlockBehaviour : MonoBehaviour
 {
     public GameObject shotPoint;
     public GameObject bullet;
+    public float bulletSpeed = 10f;
     Rigidbody rb;
+    bool canShoot = true;
     void Start()
     {
         rb = bullet.GetComponent<Rigidbody>();
@@ -17,11 +19,23 @@ public class FlintlockBehaviour : MonoBehaviour
     }
     public void Shoot()
     {
-        GameObject newBullet = Instantiate(bullet, shotPoint.transform.position, shotPoint.transform.rotation);
-        newBullet.GetComponent<Rigidbody>().linearVelocity = transform.forward * 10;
+        if (canShoot)
+        {
+            GameObject newBullet = Instantiate(bullet, shotPoint.transform.position, shotPoint.transform.rotation);
+            newBullet.GetComponent<Rigidbody>().linearVelocity = transform.forward * bulletSpeed;
+            StartCoroutine(DespawnBullet(newBullet));
+            StartCoroutine(ReloadTimer());
+            canShoot = false;
+        }
     }
-    //public IEnumerator DespawnBullet()
-    //{
-
-    //}
+    public IEnumerator DespawnBullet(GameObject bullet)
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(bullet);
+    }
+    public IEnumerator ReloadTimer()
+    {
+        yield return new WaitForSeconds(3);
+        canShoot = true;
+    }
 }
